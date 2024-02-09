a07f4d4652a62e988ca2d46ae4eb054c0241a6e1
Vania Lavielle Castro <vlavielle@soaint.com>
2024-02-09 11:10:01 -0500

    Estamos en la presentacion, con tilde

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('<h1>Hola equipo - Test CI/CD - Estamos en la presentacion <h1/>')
+    res.end('<h1>Hola equipo - Test CI/CD - Estamos en la presentación <h1/>')
 })
 const PORT = process.env.PORT || 8081
 server.listen(PORT, () => console.log('Listening'))
